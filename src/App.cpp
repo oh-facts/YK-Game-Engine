f6ba83cf73fc0c337553bcacb2130aa80e7817e7
oh-facts <as17793@nyu.edu>
2023-08-11 19:03:48 +0530

    color blending rectangle

--- src/App.cpp
+++ src/App.cpp
@@ -64,10 +64,10 @@ namespace AE
       glCompileShader(vertexShader);
 
       float vertices[] = {
-          0.5f, 0.5f, 0.0f,       1.0f,0.0f,0.0f,                // top right
-          0.5f, -0.5f, 0.0f,      0.0f,1.0f,0.0f,                // bottom right
-          -0.5f, -0.5f, 0.0f,     0.0f,0.0f,1.0f,                // bottom left
-          -0.5f, 0.5f, 0.0f,      0.0f,1.0f,0.0f,                // top left
+          0.5f, 0.5f, 0.0f,          1.0f,0.875f,0.0f,             // top right
+          0.5f, -0.5f, 0.0f,         1.0f,0.564f,0.784f,               // bottom right
+          -0.5f, -0.5f, 0.0f,        1.0f,0.875f,0.0f,               // bottom left
+          -0.5f, 0.5f, 0.0f,          0.129f,0.329f, 0.647f,                // top left
       };
 
       unsigned int indices[] = {
@@ -91,10 +91,10 @@ namespace AE
       glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
       //pos attrib
-      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
       glEnableVertexAttribArray(0);
 
-      glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)(3* sizeof(float)));
+      glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3* sizeof(float)));
       glEnableVertexAttribArray(1);
 
       glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -127,10 +127,6 @@ namespace AE
 
         glUseProgram(shaderProgram);
 
-      
-
-        float timeValue = glfwGetTime();
-        float colorValue = sin(timeValue) / 2.0f + 0.5f;
       
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
