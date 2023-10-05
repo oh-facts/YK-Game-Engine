eb95efc742abde7239993b24d06adb29447a9cb2
oh-facts <as17793@nyu.edu>
2023-10-05 02:04:02 -0400

    pruning.

--- include/yk/physics/yk_physics.h
+++ include/yk/physics/yk_physics.h
@@ -3,56 +3,6 @@
 #include <yk/math/yk_math.h>
 #include <yk/data_structures/yk_yektor.h>
 
-extern YK_Yektor yk_rigidbodies;
-
-typedef enum YK_Collision_dir
-{
-    COLLISION_NONE,
-    COLLISION_LEFT,
-    COLLISION_RIGHT,
-    COLLISION_TOP,
-    COLLISION_BOTTOM,
-} YK_Collision_dir;
-
-typedef struct YK_Aabb
-{
-    v2f pos;
-    v2f size;
-    YK_Collision_dir coll_dir;
-
-} YK_Aabb;
-
-typedef struct YK_Rigidbody
-{
-    v2f pos;
-    v2f velocity;
-    v2f acceleration;
-    v2f friction;
-    f4 mass;
-} YK_Rigidbody;
-
-void yk_rigidbody_add(YK_Rigidbody *out);
-
-void yk_rigidbody_innit(YK_Rigidbody *out);
-void yk_rigidbody_add_(v2f *pos, f4 mass);
-v2f yk_rigidbody_get_pos(i4 id);
-void yk_rigidbody_set_vel(i4 id, v2f *vel);
-void yk_rigidbody_add_force(i4 id, v2f *force);
-
-void yk_physics_innit();
-
-void yk_physics_update(f4 delta);
-
-b1 yk_physics_colliding(YK_Aabb *a, const YK_Aabb *b);
-
-v3f yk_physics_get_collision_dir(const YK_Aabb *aabb);
-
-void yk_physics_resolve_collision(YK_Aabb *a, const YK_Aabb *b);
-
-v3f yk_physics_get_overlap_distance(YK_Aabb *a, const YK_Aabb *b);
-
-
-
 struct YK_AABB
 {
     v2f min;
--- sandbox/main.c
+++ sandbox/main.c
@@ -1,282 +1,111 @@
 #include <yk/yk_app.h>
-#include <time.h>
 
-#define num_babbit 3
+#define NUM 1000
 
-#define POSITION_COMPONENT (1 << 0)
-#define SPRITE_COMPONENT (1 << 1)
-#define PLAYER_COMPONENT (1 << 2)
-#define COLLIDER_COMPONENT (1 << 3)
-
-typedef struct YK_Player
+struct square
 {
-  YK_Camera2d *cam;
-} YK_Player;
+    YK_Transform transform;
+};
 
-void debug_input(YK_Camera2d *cam, float delta)
+typedef struct square square;
+
+#define SPEED 4
+void update_player(square *py, f4 delta)
 {
-  float speed = 5.f * delta;
-  if (yk_input_is_key_held(YK_KEY_W))
-  {
-    yk_camera2d_move_up(cam, speed);
-  }
+    v2f mv = {0, 0};
 
-  if (yk_input_is_key_held(YK_KEY_A))
-  {
-    yk_camera2d_move_left(cam, speed);
-  }
-
-  if (yk_input_is_key_held(YK_KEY_S))
-  {
-    yk_camera2d_move_down(cam, speed);
-  }
-
-  if (yk_input_is_key_held(YK_KEY_D))
-  {
-    yk_camera2d_move_right(cam, speed);
-  }
-}
-
-/*
-  To Do: 1. Rotations
-         2. Unit Testing api
-         3. Logger
-*/
-
-YK_Yektor pos_comps;
-YK_Yektor sprite_comps;
-YK_Yektor aabb_comps;
-YK_Player py;
-
-u1 comp_mask[num_babbit];
-
-b1 has_component(int id, int type)
-{
-  return (comp_mask[id] & type) != 0;
-}
-
-void yk_ecs_sprite_render_system(YK_Renderer2d *ren)
-{
-
-  for (int i = 0; i < num_babbit; i++)
-  {
-    // printf("%d %hhd %hhd \n", i, has_component(i, POSITION_COMPONENT), has_component(i, SPRITE_COMPONENT));
-    if (has_component(i, POSITION_COMPONENT) && has_component(i, SPRITE_COMPONENT))
-    {
-      YK_Sprite *_sprite = yk_yektor_get(&sprite_comps, i);
-      v3f *_pos = yk_yektor_get(&pos_comps, i);
-
-      // printf(" %d %f %d \n", i, _pos->x, _sprite->texture.width);
-
-      // some reason, sprite data is lost. check how yektor stores shit.
-      // texture data is lost only. So weird. Fixed that. I was initializing
-      // my vectors incorrectly so there was no space for texture data
-      // so everything but that was stored. New problem, for some reason, my
-      // first texture is renderered on my second sprite, my second texture on my third
-      // sprite and my third texture on my first sprite. Since I have only been ever
-      // rendering the same thing I haven't noticed before maybe. Also, apparently,
-      // I don't need to cast my void pointer to requried types. who knew.
-
-      _sprite->transform.pos = *_pos;
-      yk_renderer2d_render_sprite(ren, _sprite);
-    }
-  }
-  // printf("\n");
-}
-
-void yk_ecs_player_system(int player, f4 delta)
-{
-  if (has_component(player, POSITION_COMPONENT) && has_component(player, PLAYER_COMPONENT))
-  {
-    v3f *_pos = yk_yektor_get(&pos_comps, player);
-
-    float speed = 5.f * delta;
     if (yk_input_is_key_held(YK_KEY_W))
     {
-      _pos->y += speed;
+        mv.y = SPEED;
     }
 
     if (yk_input_is_key_held(YK_KEY_A))
     {
-      _pos->x -= speed;
+        mv.x = -SPEED;
     }
 
     if (yk_input_is_key_held(YK_KEY_S))
     {
-      _pos->y -= speed;
+        mv.y = -SPEED;
     }
 
     if (yk_input_is_key_held(YK_KEY_D))
     {
-      _pos->x += speed;
+        mv.x = SPEED;
     }
-  }
+
+    py->transform.pos.x += mv.x * delta;
+    py->transform.pos.y += mv.y * delta;
 }
-
-void yk_ecs_physics_system(YK_Renderer2d *ren, YK_Sprite *debug_draw)
-{
-  for (int i = 0; i < 2; i++)
-  {
-    if (has_component(i, POSITION_COMPONENT) && has_component(i, COLLIDER_COMPONENT))
-    {
-      v3f *_pos = yk_yektor_get(&pos_comps, i);
-      YK_Aabb *_aabb = yk_yektor_get(&aabb_comps, i);
-
-      _aabb->pos.x = _pos->x;
-      _aabb->pos.y = _pos->y;
-
-      YK_Sprite _a = *debug_draw;
-      _a.transform.pos = *_pos;
-
-      yk_renderer2d_render_sprite(ren, &_a);
-    }
-  }
-}
-
-void yk_ecs_collide_system(f4 delta)
-{
-  for (int i = 0; i < 1; i++)
-  {
-    if (has_component(i, POSITION_COMPONENT) && has_component(i, COLLIDER_COMPONENT))
-    {
-      v3f *_pos = yk_yektor_get(&pos_comps, i);
-      YK_Aabb *_aabb = yk_yektor_get(&aabb_comps, i);
-      YK_Aabb *_aabb2 = yk_yektor_get(&aabb_comps, i + 1);
-
-      if (yk_physics_colliding(_aabb, _aabb2))
-      {
-        v3f a = yk_physics_get_overlap_distance(_aabb,_aabb2);
-        *_pos = yk_math_vec3f_sub(_pos, &a);
-      }
-    }
-  }
-}
-
-/*
-Hall of fame -
-
-&(*(YK_Vec3f *)yk_yektor_get(&pos_comps, i))
-Although I am sure the compiler optimized something that meaningless away
-*/
-
-// need some kind of check array. get works exactly as it should, not as I hoped it could.
-// But 10:30 is bed time and I am trying to be sane, so I will sleep.
-// Did some reading and watched elvis the alien video. I need sparse sets for exactly this.
-// still can't wrap around why I need sparse sets, and how SoA would be better for this.
-// I could use a bitmask (or array of booleans for easier implementation) to decide if someone
-// has a component. Maybe this will make sense later.
-// I ended up using bitmasks
 
 int main()
 {
+    YK_Window win;
+    yk_window_innit(&win);
+    yk_window_set_vsync(&win, false);
 
-  YK_Window win;
-  yk_window_innit(&win);
-  yk_window_set_vsync(&win, false);
+    YK_Camera2d cam2d;
+    yk_camera2d_innit(&cam2d);
+    cam2d.zoom = 2.f;
 
-  YK_Camera2d cam2d;
-  yk_camera2d_innit(&cam2d);
-  cam2d.zoom = 1.f;
+    YK_Renderer2d ren2d;
+    yk_renderer2d_innit(&ren2d, &cam2d, &win);
 
-  YK_Renderer2d ren2d;
-  yk_renderer2d_innit(&ren2d, &cam2d, &win);
+    f4 delta_time = 0.f;
+    f4 last_frame = 0.f;
 
-  yk_yektor_innit(&pos_comps, 10, sizeof(v3f));
-  yk_yektor_innit(&sprite_comps, 10, sizeof(YK_Sprite));
-  yk_yektor_innit(&aabb_comps, 10, sizeof(YK_Aabb));
+    yk_renderer2d_set_bg(0.2f, 0.3f, 0.3f, 1.f);
 
-  f4 delta_time = 0.f;
-  f4 last_frame = 0.f;
+    square py = {.transform = {{0, 0, -2.f}, {0, 0, 0}, {1.f, 1.f, 0}}};
 
-  yk_renderer2d_set_bg(0.2f, 0.3f, 0.3f, 1.f);
-  int gooba = yk_ecs_create_entity();
-  {
+    yk_renderer2d_set_bg(0.5f, 0.2f, 0.4f, 1.f);
 
-    YK_Sprite _sprite;
-    yk_sprite_innit(&_sprite, "yk-res/textures/default.jpg");
-    yk_yektor_push(&sprite_comps, &_sprite);
-
-    v3f _pos = {-1.f, 0.f, -1.f};
-    yk_yektor_push(&pos_comps, &_pos);
-
-    YK_Aabb _aabb;
-    _aabb.pos.x = _pos.x;
-    _aabb.pos.y = _pos.y;
-    _aabb.size.x = 1.f;
-    _aabb.size.y = 1.f;
-    yk_yektor_push(&aabb_comps, &_aabb);
-
-    comp_mask[gooba] = POSITION_COMPONENT | SPRITE_COMPONENT | COLLIDER_COMPONENT;
-  }
-
-  int player = yk_ecs_create_entity();
-  {
-
-    YK_Sprite _sprite;
-    yk_sprite_innit(&_sprite, "yk-res/textures/yk.png");
-    yk_yektor_push(&sprite_comps, &_sprite);
-
-    v3f _pos = {0.f, 0.f, -1.f};
-    yk_yektor_push(&pos_comps, &_pos);
-
-    py.cam = &cam2d;
-
-    YK_Aabb _aabb;
-    _aabb.pos.x = _pos.x;
-    _aabb.pos.y = _pos.y;
-    _aabb.size.x = 1.f;
-    _aabb.size.y = 1.f;
-    yk_yektor_push(&aabb_comps, &_aabb);
-
-    comp_mask[player] = POSITION_COMPONENT | SPRITE_COMPONENT | PLAYER_COMPONENT | COLLIDER_COMPONENT;
-  }
-
-  int gooba2 = yk_ecs_create_entity();
-  {
+    square squares[NUM];
+    for (int i = 0; i < NUM; i++)
     {
-      YK_Sprite _sprite;
-      yk_sprite_innit(&_sprite, "yk-res/textures/cullen.png");
-      yk_yektor_push(&sprite_comps, &_sprite);
+        f4 r1_x = ((f4)rand() / RAND_MAX) * 20.0f - 10.f;
+        f4 r1_y = ((f4)rand() / RAND_MAX) * 10.0f - 5.f;
+        f4 r2 = ((f4)rand() / RAND_MAX) * 90.0f;
+        f4 r3 = ((f4)rand() / RAND_MAX) * 0.5f + 0.2f;
+        YK_Transform trans = {{r1_x, r1_y, -2.f}, {0, 0, r2}, {r3, r3, 0}};
+
+        squares[i].transform = trans;
     }
 
+    YK_Texture test = yk_texture_create("yk-res/textures/yk.png");
+
+    while (yk_window_is_running(&win))
     {
-      v3f _pos = {1.f, 0.f, -1.f};
-      yk_yektor_push(&pos_comps, &_pos);
+
+        f4 current_frame = glfwGetTime();
+        delta_time = current_frame - last_frame;
+        last_frame = current_frame;
+
+        // debug_input(&cam2d, delta_time);
+
+        update_player(&py, delta_time);
+
+        yk_renderer2d_run(&ren2d, &win);
+        yk_renderer2d_render_rect_sprite(&ren2d, &py.transform, &YK_COLOR_WHITE, &test);
+
+        for (int i = 0; i < NUM; i++)
+        {
+            f4 timeValue = (glfwGetTime() + i * 0.1f) * 5.f;
+            f4 r = sin(timeValue) / 2.0f + 0.5f;
+            f4 g = sin(timeValue + 2.0f) / 2.0f + 0.5f;
+            f4 b = sin(timeValue + 4.0f) / 2.0f + 0.5f;
+
+            squares[i].transform.rot.z = timeValue * 5.f;
+            yk_renderer2d_render_rect(&ren2d, &squares[i].transform, &(YK_Color){r, g, b, 1.f});
+        }
+
+        // printf("Draw Calls: %d\n",draw_calls);
+
+        yk_window_run(&win);
     }
 
-    comp_mask[gooba2] = POSITION_COMPONENT | SPRITE_COMPONENT;
-  }
+    yk_renderer2d_cleanup();
+    yk_window_destroy(&win);
 
-  YK_Sprite _debug_draw;
-  yk_sprite_innit(&_debug_draw, "yk-res/textures/outline.png");
-
-  // get cullen to help tomorrow. add him as a sprite to help see pattern?
-
-  while (yk_window_is_running(&win))
-  {
-
-    f4 current_frame = glfwGetTime();
-    delta_time = current_frame - last_frame;
-    last_frame = current_frame;
-
-    // debug_input(&cam2d, delta_time);
-
-    yk_renderer2d_run(&ren2d, &win);
-
-    yk_ecs_player_system(player, delta_time);
-
-    yk_ecs_physics_system(&ren2d, &_debug_draw);
-    yk_ecs_collide_system(delta_time);
-
-    yk_ecs_sprite_render_system(&ren2d);
-
-    yk_window_run(&win);
-  }
-
-  yk_sprite_destroy(&_debug_draw);
-  yk_yektor_destroy(&pos_comps);
-  yk_yektor_destroy(&sprite_comps);
-  yk_window_destroy(&win);
-
-  return 0;
+    return 0;
 }
